8bfeafe58ada6218fd736bf85dda3009edcd629b
Sebas <cell333666@gmail.com>
2022-11-29 10:41:50 +0100

    Grundstruktur

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,29 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Worker worker = new Worker();
+        Queen queen = new Queen();
+        Drone drone = new Drone();
+
+        
+        output(worker.doYourJob());
+        output(drone.doYourJob());
+        output(queen.doYourJob());
+
+        output("------------------");
+
+        output(worker.fly());
+        output(drone.fly());
+        output(queen.fly());
+
+
+
+
+
+
+
+
     }
 
     public static void output(String outputStr) {
